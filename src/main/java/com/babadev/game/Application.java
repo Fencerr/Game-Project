4879d8738730e11cab66a2a4ee56ad69740b7534
lawsonwright <lawsonwright@icloud.com>
2021-04-30 22:24:48 -0400

    Added Spring :)

--- src/main/java/com/babadev/game/Application.java
+++ src/main/java/com/babadev/game/Application.java
@@ -1,14 +1,17 @@
 package com.babadev.game;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Main application
  * @author Everyone
  * @since 2021-04-30
  * @version 1.0
  */
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		SpringApplication.run(Application.class, args);
 	}
-
 }
