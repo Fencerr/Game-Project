d676206c6b232801c9f690409cc64c46a3d47db6
lawsonwright <lawsonwright@icloud.com>
2021-04-30 21:12:02 -0400

    Initial commit.

---
+++ src/main/java/com/babadev/game/Application.java
@@ -0,0 +1,14 @@
+package com.babadev.game;
+
+/**
+ * Main application
+ * @author Everyone
+ * @since 2021-02-18
+ * @version 1.0
+ */
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
